d4d93c3e316ab5e3aaaa2ccb9bc7f49cc0d79f39
Joshua Potter <jrpotter2112@gmail.com>
2023-11-30 12:57:01 -0700

    Add a general `Scraper` class.

--- app/scraper.py
+++ app/scraper.py
@@ -1,200 +1,92 @@
 import aiohttp
-import argparse
-import asyncio
+import json
 import os
-import os.path
 
-from bs4 import BeautifulSoup
-
-
-# The root directory containing downloaded files for a coach.
-DATA_COACH_DIR = "data/coach/{username}"
-
-# Where a part of coach-related data is stored.
-DATA_COACH_FILE = "data/coach/{username}/{filename}"
-
-# Where a part of all discovered coach usernames is stored.
-DATA_COACH_LIST = "data/pages/{page_no}.txt"
-
-# The "User-Agent" value set in every request to chess.com.
-USER_AGENT = "BoardWise chesscom-scraper ({user_agent})"
-
-# How long to wait between a batch of network requests.
-SLEEP_SECS = 3
+from typing import List, Union
+from typing_extensions import TypedDict
 
 
-def ANSI_COLOR(s: str):
-    """Print colored output to the console."""
-    return f"\033[0;34m{s}\033[0m"  # Blue
+Export = TypedDict(
+    "Export",
+    {
+        "fide_rapid": Union[int, None],
+    },
+)
 
 
-async def chesscom_request(session: aiohttp.ClientSession, url: str):
-    """Convenience function for network requests to chess.com.
+class Scraper:
+    def __init__(self, site: str, session: aiohttp.ClientSession):
+        """Initialize a new web scraper and exporter.
 
-    @param session
-        The `aiohttp.ClientSession` context our requests are made from.
-    @param url
-        The URL to send a request to.
-    @return
-        The text response returned by the server at @url.
-    """
-    async with session.get(url) as response:
-        if response.status == 200:
-            return await response.text()
-        print(f"Encountered {response.status} when retrieving {url}.")
+        @param site:
+            The site we are making requests out to.
+        @param session:
+            The `aiohttp.ClientSession` context our requests are made from.
+        """
+        self.site = site
+        self.session = session
 
+    async def download_usernames(self) -> List[str]:
+        """Collect all coach usernames from the specified site."""
+        raise NotImplementedError()
 
-async def _scrape_page_coach_usernames(session: aiohttp.ClientSession, page_no: int):
-    """Scan through chess.com/coaches/?page=<n> for all coaches' usernames.
+    async def download_profile(self, username: str):
+        """For each coach, download coach-specific data."""
+        raise NotImplementedError()
 
-    @param session
-        The `aiohttp.ClientSession` context our requests are made from.
-    @param page_no
-        The page consisting of at most 25 coaches (at the time of writing)
-        whose usernames are to be scraped.
-    @return
-        The list of scraped usernames on the specified coach listing page.
-    """
-    url = f"https://www.chess.com/coaches?sortBy=alphabetical&page={page_no}"
-    response = await chesscom_request(session, url)
-    if response is None:
-        return
+    async def export(self, username: str) -> Export:
+        """transform coach-specific data into uniform format."""
+        raise NotImplementedError()
 
-    usernames = []
-    soup = BeautifulSoup(response, "html.parser")
-    members = soup.find_all("a", class_="members-categories-username")
-    for member in members:
-        href = member.get("href")
-        username = href[len("https://www.chess.com/member/") :]
-        usernames.append(username)
+    async def request(self, url: str) -> (Union[str, None], int):
+        """Make network requests using the internal session.
 
-    return usernames
+        @param url
+            The URL to make a GET request to.
+        @return
+            Tuple containing the response body (if the request was successful)
+            and status code.
+        """
+        async with self.session.get(url) as response:
+            if response.status == 200:
+                return await response.text(), 200
+        return None, response.status
 
+    async def scrape(self):
+        """Main entrypoint for scraping and exporting downloaded content.
 
-async def _scrape_all_coach_usernames(
-    session: aiohttp.ClientSession, max_pages: int = 64
-):
-    """Scan through chess.com/coaches for all coaches' usernames.
+        A `Scraper` is structured to operates in the following stages:
 
-    @param session
-        The `aiohttp.ClientSession` context our requests are made from.
-    @param max_pages
-        The number of pages we will at most iterate through. This number was
-        determined by going to chess.com/coaches?sortBy=alphabetical&page=1
-        and traversing to the last page.
-    @return
-        The complete list of scraped usernames across every coach listing page.
-    """
-    usernames = []
-    for page_no in range(1, max_pages + 1):
-        filepath = DATA_COACH_LIST.format(page_no=page_no)
-        try:
-            with open(filepath, "r") as f:
-                usernames.extend(f.readlines())
-            print(f"Skipping {ANSI_COLOR(filepath)}")
-        except FileNotFoundError:
-            page_usernames = await _scrape_page_coach_usernames(session, page_no)
-            if not page_usernames:
-                print(f"Could not write {ANSI_COLOR(filepath)}")
-                continue
-            with open(filepath, "w") as f:
-                for username in page_usernames:
-                    f.write(f"{username}\n")
-            usernames.extend(page_usernames)
-            print(f"Downloaded {ANSI_COLOR(filepath)}")
-            await asyncio.sleep(SLEEP_SECS)
+        1. Collect all coach usernames from the specified site.
+        2. For each coach, download coach-specific data.
+        3. Transform this data and export into uniform format.
+        """
+        os.makedirs(self.path_coaches_dir(), exist_ok=True)
+        os.makedirs(self.path_pages_dir(), exist_ok=True)
+        usernames = self.download_usernames()
+        for username in usernames:
+            os.makedirs(self.path_coach_dir(username), exist_ok=True)
+            await self.download_profile(username)
+            export = await self.export(username)
+            with open(self.path_coach_file(username, "export.json")) as f:
+                json.dump(export, f)
 
-    return usernames
+    def path_coaches_dir(self):
+        """The root directory for all coach-related downloads."""
+        return os.path.join("data", self.site, "coach")
 
+    def path_coach_dir(self, username: str):
+        """The root directory for a specific coach's downloads."""
+        return os.path.join(self.path_coaches_dir(), username)
 
-async def _download_coach_file(
-    session: aiohttp.ClientSession, url: str, username: str, filename: str
-):
-    """Writes the contents of @url into `DATA_COACH_FILE`.
+    def path_coach_file(self, username: str, filename: str):
+        """Path to a coach-specific file download."""
+        return os.path.join(self.path_coach_dir(username), filename)
 
-    @param session
-        The `aiohttp.ClientSession` context our requests are made from.
-    @param url
-        The URL of the file to download.
-    @param username
-        The coach username corresponding to the downloaded file.
-    @param filename
-        The output file to write the downloaded content to.
-    @return:
-        True if we make a network request. False otherwise.
-    """
-    filepath = DATA_COACH_FILE.format(username=username, filename=filename)
-    if os.path.isfile(filepath):
-        return False
+    def path_pages_dir(self):
+        """The root directory for all username listing files."""
+        return os.path.join("data", self.site, "pages")
 
-    response = await chesscom_request(session, url)
-    if response is not None:
-        with open(filepath, "w") as f:
-            f.write(response)
-    return True
-
-
-async def _download_coach_data(session: aiohttp.ClientSession, username: str):
-    """Download coach-related data to the `DATA_COACH_DIR` directory.
-
-    This sends three parallel requests for:
-    * the coach's profile,
-    * the coach's recent activity,
-    * the coach's stats.
-
-    @param session
-        The `aiohttp.ClientSession` context our requests are made from.
-    @param username
-        The coach username corresponding to the downloaded files.
-    """
-    used_network = await asyncio.gather(
-        _download_coach_file(
-            session,
-            url=f"https://www.chess.com/member/{username}",
-            username=username,
-            filename=f"{username}.html",
-        ),
-        _download_coach_file(
-            session,
-            url=f"https://www.chess.com/callback/member/activity/{username}?page=1",
-            username=username,
-            filename="activity.json",
-        ),
-        _download_coach_file(
-            session,
-            url=f"https://www.chess.com/callback/member/stats/{username}",
-            username=username,
-            filename="stats.json",
-        ),
-    )
-    if any(used_network):
-        print(f"Downloaded {ANSI_COLOR(username)}")
-        await asyncio.sleep(SLEEP_SECS)
-    else:
-        print(f"Skipping {ANSI_COLOR(username)}")
-
-
-async def _scrape():
-    parser = argparse.ArgumentParser(
-        prog="chesscom-scraper",
-        description="HTML scraping of chess.com coaches.",
-    )
-    parser.add_argument("-u", "--user-agent", required=True)
-    args = parser.parse_args()
-
-    os.makedirs("data/pages", exist_ok=True)
-    os.makedirs("data/coach", exist_ok=True)
-
-    async with aiohttp.ClientSession(
-        headers={"User-Agent": USER_AGENT.format(user_agent=args.user_agent)}
-    ) as session:
-        # Retrieve all coaches on the platform.
-        usernames = await _scrape_all_coach_usernames(session)
-        # For each coach, download relevant data.
-        for username in [u.strip() for u in usernames]:
-            os.makedirs(DATA_COACH_DIR.format(username=username), exist_ok=True)
-            await _download_coach_data(session, username)
-
-
-def run():
-    asyncio.run(_scrape())
+    def path_page_file(self, page_no: int):
+        """The root directory for usernames scraped from a single page."""
+        return os.path.join(self.path_pages_dir(), str(page_no))
